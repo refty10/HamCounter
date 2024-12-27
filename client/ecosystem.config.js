ea0507ec0552e52bd2ce37b383f5d0b3e8164833
refty10 <46354485+refty10@users.noreply.github.com>
2024-12-27 20:59:52 +0900

    chore: pm2で自動的にリスタートするように

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: "ham-client",
       script: "python main.py https://ham-server.refty.tech",
+      watch: true, // ファイル変更時に自動再起動
+      autorestart: true, // 自動再起動を有効化
     },
   ],
 };
